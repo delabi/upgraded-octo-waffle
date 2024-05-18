017f941a03b42b1a3df9a9b0ca4479656adb771c
Laban Bwire <bwire.laban@gmail.com>
2024-05-18 19:32:06 +0300

    Routes mastery

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,21 @@ use App\Models\Message;
 use App\Models\Post;
 use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// Route::controller(JobController::class)->group(function(){
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+Route::resource('jobs', JobController::class);
+
+
 
 Route::get('/messages', function (){
     return view('messages', [
@@ -35,10 +39,7 @@ Route::get('/posts', function () {
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about', 'about');
 
-Route::get('/contact', function () {
-        return view('contact');
-});
+
+Route::view('/contact', 'contact');
