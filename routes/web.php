72f53fb3be8b5266ce89a97071c5ec9276d860f4
Laban Bwire <bwire.laban@gmail.com>
2024-05-18 17:47:59 +0300

    can edit and delete jobs

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3); //paginate(3);
 
@@ -19,19 +20,26 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view ('jobs.create');
 
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function(){
-    // dd(request()->all());
-    //Validate
+
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
@@ -41,6 +49,44 @@ Route::post('/jobs', function(){
     return redirect('/jobs');
 });
 
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorization(on hold)
+
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+
+    // authorize
+    // delete job
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+
+});
 
 Route::get('/messages', function (){
     return view('messages', [
